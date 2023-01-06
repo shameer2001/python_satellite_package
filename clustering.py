e33ba0f879375a2f5e9911ac7f57023e55185007
Qiang Hu <ucapqhu@ucl.ac.uk>
2023-01-06 16:33:32 +0000

    Add functions 'cluster' and 'distance' to make it readable;

--- clustering.py
+++ clustering.py
@@ -16,27 +16,37 @@ def load_data(filepath: Path):
 
 points = load_data(Path("samples.csv"))
 
-centres = [points[randrange(len(points))], points[randrange(len(points))], points[randrange(len(points))]]
 
-alloc = [None] * len(points)
-count = 0
-while count < 10:
-    for i in range(len(points)):
-        point = points[i]
-        dist = [None] * 3
-        dist[0] = sqrt((point[0] - centres[0][0]) ** 2 + (point[1] - centres[0][1]) ** 2 + (point[2] - centres[0][2]) ** 2)
-        dist[1] = sqrt((point[0] - centres[1][0]) ** 2 + (point[1] - centres[1][1]) ** 2 + (point[2] - centres[1][2]) ** 2)
-        dist[2] = sqrt((point[0] - centres[2][0]) ** 2 + (point[1] - centres[2][1]) ** 2 + (point[2] - centres[2][2]) ** 2)
-        alloc[i] = dist.index(min(dist))
+def distance(point1: tuple, point2: tuple):
+    dist = sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2 + (point1[2] - point2[2]) ** 2)
+    return dist
+
+
+def cluster(data: list, iterations: int):
+    num = len(data)
+    centres = [data[randrange(num)], data[randrange(num)], data[randrange(num)]]
+
+    alloc = [None] * num
+    count = 0
+    while count < iterations:
+        for i in range(num):
+            point = data[i]
+            dist = [None] * 3
+            dist[0] = distance(point, centres[0])
+            dist[1] = distance(point, centres[1])
+            dist[2] = distance(point, centres[2])
+            alloc[i] = dist.index(min(dist))
+        for i in range(3):
+            alloc_ps = [p for j, p in enumerate(data) if alloc[j] == i]
+            new_mean = (sum([a[0] for a in alloc_ps]) / len(alloc_ps),
+                        sum([a[1] for a in alloc_ps]) / len(alloc_ps), sum([a[2] for a in alloc_ps]) / len(alloc_ps))
+            centres[i] = new_mean
+        count = count + 1
     for i in range(3):
         alloc_ps = [p for j, p in enumerate(points) if alloc[j] == i]
-        new_mean = (sum([a[0] for a in alloc_ps]) / len(alloc_ps), sum([a[1] for a in alloc_ps]) / len(alloc_ps),
-                    sum([a[2] for a in alloc_ps]) / len(alloc_ps))
-        centres[i] = new_mean
-    count = count + 1
+        print("Cluster " + str(i) + " is centred at " + str(centres[i]) + " and has " + str(len(alloc_ps)) + " points.")
 
-for i in range(3):
-    alloc_ps = [p for j, p in enumerate(points) if alloc[j] == i]
-    print("Cluster " + str(i) + " is centred at " + str(centres[i]) + " and has " + str(len(alloc_ps)) + " points.")
+        print(alloc_ps)
 
-    print(alloc_ps)
+
+cluster(points, 20)
