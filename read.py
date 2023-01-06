81eb1137a2da17766a4e2c9981c2def183d49627
Shameer <shameer.ahmad.19@ucl.ac.uk>
2023-01-06 20:57:09 +0000

    Added doctrings and error messages to read() function.

--- read.py
+++ read.py
@@ -9,9 +9,147 @@ import os
 from io import BytesIO
 
 def read(file):
+    """Extracts the data and meta data from the input file.
+
+    Parameters
+    ----------
+    file: str
+          The name of the file or path to the file. 
+    
+    Notes
+    -----
+    
+    Each of the instruments used different file formats to store the data and meta data:
+
+    Lir uses Advanced Scientific Data Format (ASDF). File extension: '.asdf'   
+    Manannn uses Hierarchical Data Format 5 (HDF5). 
+    Fand stores the data in 'npy' format and the meta data in JSON files
+    Ecne measures turbulence, salinity and algal density and stores the data in CSV format
 
 
 
+    To understand this function better, the content structure of the types of files are detailed below:
+
+    The structure of the '.asdf' files:
+
+    root (asdf object)
+    ├─asdf_library (Software)
+    │ ├─author (str): The ASDF Developers
+    │ ├─homepage (str): http://github.com/asdf-format/asdf
+    │ ├─name (str): asdf
+    │ └─version (str): 2.14.2
+    ├─history (dict)
+    │ └─extensions (list)
+    │   └─[0] (ExtensionMetadata) ...
+    ├─archive (str): ISA
+    ├─data (NDArrayType): shape=(val, val), dtype=float64   
+    ├─date (str): YYYY-mm-dd
+    ├─instrument (str): Lir
+    ├─observatory (str): Aigean
+    ├─resolution (int): 30
+    ├─time (str): hh:mm:ss
+    ├─xcoords (list)
+    │ ├─[0] (float): val
+    │ └─[1] (float): val
+    ├─ycoords (list)
+    │ ├─[0] (float): val
+    │ └─[1] (float): val
+    └─year (int): YYYY
+
+
+
+    The structure of the '.hdf5' files:
+
+    root (hdf5 object)
+    |─attrs:
+      |─archive (str) = 'ISA'
+      |─year (int) = YYYY
+    observation
+    |─ attrs:
+       |─date (str): 'YYYY-mm-dd'
+       |─instrument (str): 'Manannan'
+       |─observatory (str): 'Aigean'
+       |─resolution (int): 15
+       |─time (str); 'hh:mm:ss'
+       |─xcoords (arr) 
+         ├─[0] (float): val
+         └─[1] (float): val
+       |─ycoords (arr)
+         ├─[0] (float): val
+         └─[1] (float): val
+    |─data (NDArrayType): float64(val x val)
+
+    
+    The structure of the '.json' files:
+    
+    dictionary 
+    |─archive (str): "ISA"
+    |─year (int): YYYY
+    |─observatory (str): "Aigean"
+    |─instrument (str): "Fand"
+    |─date (str): "YYYY-mm-dd"
+    |─time (str): "hh-mm-ss"
+    |─xcoords (list) 
+      ├─[0] (float): val
+      └─[1] (float): val
+    |─ycoords (list)
+      ├─[0] (float): val
+      └─[1] (float): val
+    |─resolution (int): 5
+
+
+    The structure of the '.npy' files:
+
+    Array (NDArrayType)
+    |─shape=(val, val), dtype=float64 
+
+
+
+    The structure of the '.csv' files:
+    
+    Shape: 300 rows, 3 columns
+    Columns: turbulence, salinity, algal density
+
+
+
+    Returns
+    -------
+
+    For '.asdf', '.hdf5' and '.zip' files:
+
+    data: ndarray
+          Image data taken with the Lir, Manannan or Fand instrument for ASDF, HDF5 and ZIP files respectivley.
+    meta_data: dict
+               Other information about the data. This includes archive it's stored in, year, observatory, instrument, date when taken, time when taken, xcoords, ycoords, resolution. For ASDF files (ie Lir instrument data) the information about the asdf library is also included
+
+
+    For '.csv' files:
+
+    turbulence: list
+                300 turbulence measurements taken from the 300 deepest points in an area around the target region. 
+    salinity: list
+              300 salinity measurements taken from the 300 deepest points in an area around the target region. 
+    algal_density: list
+                   300 density measurements of algae taken from the 300 deepest points in an area around the target region. 
+
+               
+    
+
+
+    
+    
+    """
+
+    # Errors:
+    if type(file) != str:
+        raise TypeError("")
+
+    if not Path(file).exists(): # if no such path/file exists
+        raise FileNotFoundError("")
+
+
+
+        
 
     filetype = os.path.splitext(file)[1] # obtain file extension
 
@@ -192,3 +330,25 @@ import nexusformat.nexus as nx
 
 
 jsonnn = json.load(BytesIO(lol2))
+# for i in jsonnn:
+#     print(i)
+
+print(type(read('aigean_lir_20221205_191610.asdf')[0]))
+print(type(read('aigean_lir_20221205_191610.asdf')[1]))
+
+
+
+print(type(read('aigean_man_20221206_181924.hdf5')[0]))
+print(type(read('aigean_man_20221206_181924.hdf5')[1]))
+
+
+
+
+print(type(read('aigean_fan_20221206_190424.zip')[0]))
+print(type(read('aigean_fan_20221206_190424.zip')[1]))
+
+
+
+print(type(read('aigean_ecn_20221205_191610.csv')[0]))
+print(type(read('aigean_ecn_20221205_191610.csv')[1]))
+print(type(read('aigean_ecn_20221205_191610.csv')[2]))
