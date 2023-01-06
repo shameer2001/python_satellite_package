d61db4a51eed7ac5156ca3f66dbb402cd588dae2
Shameer <shameer.ahmad.19@ucl.ac.uk>
2023-01-06 16:14:43 +0000

    Completed query_isa() function to output meta date for measurements in a given timeframe and for a given instrument.

--- net.py
+++ net.py
@@ -1,5 +1,6 @@
+from requests import *
 
-class ISA:
+class net:
     def __init__(self, date, filename, instrument, resolution, time, xcoords, ycoords):
         self.date = date
         self.fileName = filename
@@ -10,15 +11,18 @@ class ISA:
         self.yCoords = ycoords
 
 
-def query_isa(start_date, stop_date, instrument) -> list[ISA]:
-    # todo: Query the files from https://dokku-app.dokku.arc.ucl.ac.uk/isa-archive/
-    #  return value shall be the list of ISA class
-    ...
+def query_isa(start_date = None, stop_date = None, instrument = None) -> list[net]:
+
+    payload = {'start_date':start_date, 'stop_date':stop_date, 'instrument': instrument}
+    r=get('https://dokku-app.dokku.arc.ucl.ac.uk/isa-archive/query/', params=payload)
+
+    return r.json()
 
 
-def download_isa(filename, save_dir) -> None:
+def download_isa(filename, save_dir = None) -> None:
     # todo: Download the files from https://dokku-app.dokku.arc.ucl.ac.uk/isa-archive/
     ...
 
 
-
+print(query_isa("2022-12-05", "2022-12-06", "lir") )
+#print(type(query_isa("2022-12-05", "2022-12-06", "lir") ))
