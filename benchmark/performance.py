43bc8351ed9213fdbb92c30286b0fd1536b61092
Qiang Hu <ucapqhu@ucl.ac.uk>
2023-01-14 19:57:35 +0000

    Add code for plotting and saving the figure.

--- benchmark/performance.py
+++ benchmark/performance.py
@@ -25,3 +25,15 @@ N = np.linspace(100, 10000, 10, dtype=int)
 for i in range(10):
     T_np[i] = timeit(lambda: with_np(data_full[:N[i]]), number=1)
     T_non[i] = timeit(lambda: without_np(data_full[:N[i]]), number=1)
+
+plt.figure()
+plt.scatter(N, T_np)
+plt.plot(N, T_np, label='clustering_numpy')
+plt.scatter(N, T_non)
+plt.plot(N, T_non, 'r-', label='clustering')
+plt.title('Performance for versions with and without NumPy')
+plt.xlabel('Number of points N')
+plt.ylabel('Time required T')
+plt.legend()
+save_name = 'performance.png'
+plt.savefig(save_name)
