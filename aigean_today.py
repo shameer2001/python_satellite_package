b92d74289e81afa243c359a52fa2750d58f41fab
Albert Yin <ucapjyi@ucl.ac.uk>
2023-01-14 17:04:51 +0000

    complete aigean_today.py with documentation and finish save the lastest image function, and CLI

--- aigean_today.py
+++ aigean_today.py
@@ -1,52 +1,48 @@
-import sys
 from argparse import ArgumentParser
 import requests
-import json
 from datetime import date
 from datetime import timedelta
-# from net import query_isa, download_isa
-# from satmap import get_satmap
+from net import net
+from satmap import SatMap, get_satmap
 
 
-def aigean_today(instrument = '', saveplot=False):
-    """
-    Getting the latest image of the archive
+def aigean_today(instrument = None, saveplot=False):
+    """ Getting the latest image of the archive
+
+    Parameters
     ---------
-    instrument: str
-        An optional specification: choose for 4 different intruments('lir','manannan','fand','ecne'), or leave it along as None for the last observation(with any instrument)
-    saveplot: bool
-        Ture for save the png image(except with the instrument 'ecne'), False for not save the png image. (False as default)
-    Returns
+    instrument: str, optional
+        An optional specification: choose from 4 different intruments('lir','manannan','fand','ecne'), or leave it along as None to get the last 
+        observation(with no specific instrument requirement)
+    saveplot: bool, optional
+        The save = False as default setting. If save is set to True, then the image should not be displayed on the screen and saved in the required
+        path. If save is set to False, the image should be displayed on the screen, but not get saved in the directory.
+    
+    Notes
     ---------
-        PNG_figure and data downloaded in the local directory
+    The data file and image(if there is one) will be saved in the directory where it is run.
     """
     # use 'datetime' from python standard library to get the date of today and the most recent update date
     # Cause sometimes the achive don't update constantly for several date, so we have to get the most recent date with updates
-    # if type(instrument) != str:
-    #     raise TypeError("names of instrument inpute must be string")
-    # print(instrument)
     if instrument != None:
+        if type(instrument) != str:
+            raise TypeError("name of instrument inpute must be string")
         if instrument not in ('lir','manannan','fand','ecne'):
             raise ValueError("names of instrument inpute is not available")
     d = 0
     text = {}
     while len(text) < 4:
         today = date.today()
+        # find the most recent date with data updating 
         most_recent_date = today - timedelta(days = d)
         # Set params in the request, if the instrument is not specified then it will download the most recent obsevation
-        # the reason why we also put yesterday's date is because
         if instrument != None:
-            ## use the function from net.py after commit
-            #response = query_isa(most_recent_date, most_recent_date, instrument)
-
             param = {'start_date':most_recent_date,'stop_date':most_recent_date,'instrument':str(instrument)}
             response = requests.get(
                 "http://dokku-app.dokku.arc.ucl.ac.uk/isa-archive/query/?",
                 params=param)
             text = response.text
         else:
-            ## use the function from net.py after commit
-            # response = query_isa(most_recent_date, most_recent_date,'')
             param = {'start_date':most_recent_date,'stop_date':most_recent_date}
             response = requests.get(
                 "http://dokku-app.dokku.arc.ucl.ac.uk/isa-archive/query/?",
@@ -60,25 +56,43 @@ def aigean_today(instrument = '', saveplot=False):
         date_list.append(most_recent_date_update[i]['time'])
     most_recent_obv_index = date_list.index(max(date_list))
     most_recent_obv = most_recent_date_update[most_recent_obv_index]
-    # print(type(most_recent_obv))
-    # print(len(info))
-    # print(info[15]['date'])
-    download_file = requests.get('http://dokku-app.dokku.arc.ucl.ac.uk/isa-archive/download/?',
-                    params= {'filename':most_recent_obv['filename']})
-    ## use the function from net.py after shameer commit, it should save the downloaded file rightaway
-    # download_file = download_isa (most_recent_obv['filename'])
-    
+    # download to current working directory
+    download_file = net.download_isa (most_recent_obv['filename'])
+
     if saveplot == True:  
         if most_recent_obv['instrument'] != 'ecne':
-            # wait for the visulise function
-            print('save the last image')
+            read_file = get_satmap(most_recent_obv['filename'])
+            # extract all key data from file
+            meta = read_file.meta
+            data = read_file.data
+            fov = read_file.fov
+            shape = read_file.shape
+            centre = read_file.centre
+            satmap_file = SatMap(meta=meta, data=data, shape=shape, fov=fov, centre=centre)
+            # save the image in the directory where it's run
+            save_image = satmap_file.visualise(True,'')
+            print ('message: the file downloaded can be visulised, the image is downloaded in the directory where it is run')
+            return
         else:            
-            print ('message: the file downloaded cannot be visulised')
+            return print ('message: the file downloaded cannot be visulised')
     else:
-        # save the download
-        print('save the download in directory')
-    return
-# aigean_today('ecne',saveplot=True)
+        # if the --saveplot flag is not passed, we try to show the plot if the file is visualble, and download data in the directory where it is run.
+        if most_recent_obv['instrument'] != 'ecne':
+            read_file = get_satmap(most_recent_obv['filename'])
+            meta = read_file.meta
+            data = read_file.data
+            fov = read_file.fov
+            shape = read_file.shape
+            centre = read_file.centre
+            satmap_file = SatMap(meta=meta, data=data, shape=shape, fov=fov, centre=centre)
+            # save the image in the directory where it's run
+            show_image = satmap_file.visualise(False,'')
+            return print('message: the file downloaded can be visulised, the image is showed')
+        else:
+            return print ('message: the file downloaded cannot be visulised')
+
+# test sample
+# aigean_today(instrument='lir',saveplot=True)
 
 def process_today():
     parser = ArgumentParser(description='Generate the last observation data ot image')
@@ -88,8 +102,6 @@ def process_today():
     
     args_today = parser.parse_args()
     obv = aigean_today(args_today.instrument, args_today.saveplot)
-    print(obv)
-
 
  
 
