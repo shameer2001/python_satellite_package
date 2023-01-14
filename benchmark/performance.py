df91fe41b5c57ef1356271adfda991c05b8dae40
Qiang Hu <ucapqhu@ucl.ac.uk>
2023-01-14 21:59:23 +0000

    Add function save_figure.

--- benchmark/performance.py
+++ benchmark/performance.py
@@ -18,22 +18,27 @@ def without_np(data: np.array, clusters: int = 3, iterations: int = 10):
     pass
 
 
-T_np, T_non = np.zeros(10), np.zeros(10)
-data = clustering_numpy.load_data('../samples.csv')
-data_full = np.array(list(data) * 34)
-N = np.linspace(100, 10000, 10, dtype=int)
-for i in range(10):
-    T_np[i] = timeit(lambda: with_np(data_full[:N[i]]), number=1)
-    T_non[i] = timeit(lambda: without_np(data_full[:N[i]]), number=1)
+def save_figure(filename):
+    T_np, T_non = np.zeros(10), np.zeros(10)
+    data = clustering_numpy.load_data(filename)
+    data_full = np.array(list(data) * 34)
+    N = np.linspace(100, 10000, 10, dtype=int)
+    for i in range(10):
+        T_np[i] = timeit(lambda: with_np(data_full[:N[i]]), number=1)
+        T_non[i] = timeit(lambda: without_np(data_full[:N[i]]), number=1)
 
-plt.figure()
-plt.scatter(N, T_np)
-plt.plot(N, T_np, label='clustering_numpy')
-plt.scatter(N, T_non)
-plt.plot(N, T_non, 'r-', label='clustering')
-plt.title('Performance for versions with and without NumPy')
-plt.xlabel('Number of points N')
-plt.ylabel('Time required T')
-plt.legend()
-save_name = 'performance.png'
-plt.savefig(save_name)
+    plt.figure()
+    plt.scatter(N, T_np)
+    plt.plot(N, T_np, label='clustering_numpy')
+    plt.scatter(N, T_non)
+    plt.plot(N, T_non, 'r-', label='clustering')
+    plt.title('Performance for versions with and without NumPy')
+    plt.xlabel('Number of points N')
+    plt.ylabel('Time required T')
+    plt.legend()
+    save_name = 'performance.png'
+    plt.savefig(save_name)
+
+
+if __name__ == "__main__":
+    save_figure('../samples.csv')
