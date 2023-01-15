7e8dce06c571b926c435f45bb4840bedc1c93a6b
Qiang Hu <ucapqhu@ucl.ac.uk>
2023-01-15 18:16:30 +0000

    Add negative test for kmean() which fails on non-positive clusters or iterations.

--- test/test_analysis.py
+++ test/test_analysis.py
@@ -21,3 +21,10 @@ def test_kmeans_fails_on_non_integer_clusters():
 def test_kmeans_fails_on_non_integer_iterations():
     with raises(TypeError, match="iterations must be an integer"):
         kmeans("samples.csv", 3, 14.3)
+
+
+def test_kmeans_fails_on_non_positive_clusters_and_iterations():
+    with raises(TypeError, match="clusters and iterations must be positive"):
+        kmeans("samples.csv", -3, 14)
+    with raises(TypeError, match="clusters and iterations must be positive"):
+        kmeans("samples.csv", 3, -14)
