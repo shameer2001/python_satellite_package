3d93edcb22758e6b9caab5cc5895b33b64466da8
Qiang Hu <ucapqhu@ucl.ac.uk>
2023-01-15 13:56:24 +0000

    Add docstrings and comments for performance.py

--- benchmark/performance.py
+++ benchmark/performance.py
@@ -1,6 +1,7 @@
 import numpy as np
 from timeit import timeit
 from matplotlib import pyplot as plt
+from pathlib import Path
 import sys
 
 sys.path.append('..')
@@ -8,25 +9,67 @@ import clustering_numpy
 import clustering
 
 
+# Version of cluster with numpy
 def with_np(data: np.array, clusters: int = 3, iterations: int = 10):
+    """
+    Perform the numpy version where return is unnecessary
+
+    Parameters
+    ----------
+    data: np.array
+        Dataset loaded from the csv file, which is an array
+
+    clusters : int, optional
+        Number of clusters to divide the dataset into, default is 3
+
+    iterations : int, optional
+        Number of iterations to upload the centres, default is 10
+    """
     alloc, centres = clustering_numpy.cluster(data, clusters, iterations)
     pass
 
 
+# Version of cluster without numpy
 def without_np(data: np.array, clusters: int = 3, iterations: int = 10):
+    """
+    Perform the without numpy version where return is unnecessary
+
+    Parameters
+    ----------
+    data : np.array
+        Dataset loaded from the csv file, which is an array
+
+    clusters : int, optional
+        Number of clusters to divide the dataset into, default is 3
+
+    iterations : int, optional
+        Number of iterations to upload the centres, default is 10
+    """
     alloc, centres = clustering.cluster(data, clusters, iterations)
     pass
 
 
-def save_figure(filename):
+# Compare the performance of above two function
+def save_figure(filename: Path):
+    """
+    Load dataset from the csv file and use those data to measure the performance
+
+    Parameters
+    ----------
+    filename : Path
+        A given Path, such as '../samples.csv'
+    """
     T_np, T_non = np.zeros(10), np.zeros(10)
     data = clustering_numpy.load_data(filename)
+    # Enlarge dataset by 30 times
     data_full = np.array(list(data) * 34)
+    # Set ten different sizes of dataset.
     N = np.linspace(100, 10000, 10, dtype=int)
     for i in range(10):
         T_np[i] = timeit(lambda: with_np(data_full[:N[i]]), number=1)
         T_non[i] = timeit(lambda: without_np(data_full[:N[i]]), number=1)
 
+    # Using a single plot for both versions
     plt.figure()
     plt.scatter(N, T_np)
     plt.plot(N, T_np, label='clustering_numpy')
