d0ae4977fbf8c18f1ff7da54efc6dfb749648470
Morphling2ooo <115083497+Morphling2ooo@users.noreply.github.com>
2023-01-15 15:11:24 +0000

    create aigean_mosaic with Command Line Interface

--- aigean_mosaic.py
+++ aigean_mosaic.py
@@ -1,23 +1,45 @@
 import sys
-from argparse import ArgumentParser
-import requests
-import json
-from datetime import date
-from datetime import timedelta
-from satmap_copy import SatMap
-from net_copy import net
+import argparse
+from satmap import get_satmap
 
-def aigean_mosaic (filenames, resoltion = None):
-    pass
+def aigean_mosaic():
+    parser = argparse.ArgumentParser(description='Mosaic multiple Aigean images')
+    parser.add_argument('filelist', metavar='file', type=str, nargs='+',
+                        help='List of files to be used for mosaic')
+    parser.add_argument('-r', '--resolution', type=int, default=30,
+                        help='Resolution of the final mosaic')
+    args = parser.parse_args()
 
-def process_mosaic():
-    parser = ArgumentParser(description='Creating a mosaic from the command line')
-    parser.add_argument('--resolution', help= 'resolution argument')
-    parser.add_argument('filename',type=str,nargs='+',help='input two or more file')    
-    args_mosaic = parser.parse_args()
-    obv = aigean_mosaic(args_mosaic.resolution, args_mosaic.filename)
+    files = args.filelist
+    resolution = args.resolution
 
+    
+    # check if all the files passed are valid Aigean images
+    invalid_files = []
+    satmaps = []
+    for file in files:
+        try:
+            satmap = get_satmap(file)
+            satmaps.append(satmap)
+        except:
+            invalid_files.append(file)
+
+    if invalid_files:
+        print("These files are not valid Aigean images:")
+        for file in invalid_files:
+            print(file)
+
+    if len(satmaps) == 0:
+        sys.stderr.write('No valid Aigean images')
+        sys.exit(1)
+
+    # mosaic the images
+    mosaic_map = satmaps[0]
+    for i in range(1, len(satmaps)):
+        mosaic_map = mosaic_map.mosaic(satmaps[i], resolution)
+
+    # save the mosaic
+    mosaic_map.visualise(save=True)
 
 if __name__ == "__main__":
-    process_mosaic()
-    
+    aigean_mosaic()
