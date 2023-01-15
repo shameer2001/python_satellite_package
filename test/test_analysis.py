a3b95a7c169825139851d69d7a9c77a297f3782d
Qiang Hu <ucapqhu@ucl.ac.uk>
2023-01-15 18:58:12 +0000

    Add a unit test for function kmeans() to determine whether this algorithm divides the dataset into 3 clusters (or the clusters provided).

--- test/test_analysis.py
+++ test/test_analysis.py
@@ -1,3 +1,4 @@
+import numpy as np
 from pytest import raises
 from analysis import kmeans
 
@@ -10,23 +11,29 @@ def test_kmeans_fails_on_wrong_format_filename():
 
 def test_kmeans_fails_on_non_csv_file():
     with raises(TypeError, match="The file inputted must be a csv file"):
-        kmeans("samples.txt")
+        kmeans("../samples.txt")
 
 
 def test_kmeans_fails_on_non_integer_clusters():
     with raises(TypeError, match="Clusters must be an integer"):
-        kmeans("samples.csv", 3.6)
+        kmeans("../samples.csv", 3.6)
 
 
 def test_kmeans_fails_on_non_integer_iterations():
     with raises(TypeError, match="iterations must be an integer"):
-        kmeans("samples.csv", 3, 14.3)
+        kmeans("../samples.csv", 3, 14.3)
 
 
 def test_kmeans_fails_on_non_positive_clusters_and_iterations():
     with raises(TypeError, match="clusters and iterations must be positive"):
-        kmeans("samples.csv", -3, 14)
+        kmeans("../samples.csv", -3, 14)
     with raises(TypeError, match="clusters and iterations must be positive"):
-        kmeans("samples.csv", 3, -14)
+        kmeans("../samples.csv", 3, -14)
     with raises(TypeError, match="clusters and iterations must be positive"):
-        kmeans("samples.csv", -3, -14)
+        kmeans("../samples.csv", -3, -14)
+
+
+# Unit test for kmeans
+def test_kmeans():
+    alloc = kmeans("../samples.csv")
+    assert (np.unique(alloc) == np.array([0, 1, 2])).all()
