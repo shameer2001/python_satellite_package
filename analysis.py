70c7908103ec0f11a6eeca47493ca6d7a65ef86f
Qiang Hu <ucapqhu@ucl.ac.uk>
2023-01-15 16:49:57 +0000

    Add error raising to check whether the inputs clusters and iterations are integers or not.

--- analysis.py
+++ analysis.py
@@ -28,6 +28,14 @@ def kmeans(filename: Union[Path, str], clusters: int = 3, iterations: int = 10):
     if not (isinstance(filename, Path) or isinstance(filename, str)):
         raise TypeError("The filename must be a Path or string")
 
+    # error raising: clusters using wrong format
+    if not isinstance(clusters, int):
+        raise TypeError("Clusters must be an integer")
+
+    # error raising: iterations using wrong format
+    if not isinstance(iterations, int):
+        raise TypeError("iterations must be an integer")
+
     # This function is from clustering_numpy.
     data = load_data(filename)
     # Perform the algorithm to the dataset we loaded.
@@ -36,4 +44,4 @@ def kmeans(filename: Union[Path, str], clusters: int = 3, iterations: int = 10):
     return alloc
 
 
-print(kmeans("samples.csv"))
+print(kmeans("samples.csv", 4))
