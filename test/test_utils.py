7609b106e853770138b800aa29164a0a06d883b8
Zhouhan Zhang <zhouhancheuang@gmail.com>
2023-01-17 01:55:56 +0000

    complete the test on the pixel_to_earth and earth_to_pixel functions

--- test/test_utils.py
+++ test/test_utils.py
@@ -3,91 +3,28 @@ from aigeanpy.utils import *
 import numpy as np
 import pytest
 from pytest import approx
+from aigeanpy.satmap import *
 
 ############################## Conversion between pixel and earth coord ###################################
 
 
-@pytest.mark.parametrize('pixel_coord, meta, resolution, expect_earth_coord', [
-    # the case that single pixel maps to multiple earth coords, and the resolution is provided by the meta
-    {'pixel_coord': np.array([[3, 2, 1], [4, 5, 6], [7, 8, 9]]),
-     'meta': {'archive': 'ISA', 'year': 2023, 'observatory': 'Aigean', 'instrument': 'Fand', 'date': '2022-12-05',
-              'time': '19:22:10', 'xcoords': [300.0, 309.0], 'ycoords': [50.0, 59.0], 'resolution': 3},
-     'resolution': None,
-     'expect_earth_coord': np.array(
-         [[3, 3, 3, 2, 2, 2, 1, 1, 1], [3, 3, 3, 2, 2, 2, 1, 1, 1], [3, 3, 3, 2, 2, 2, 1, 1, 1],
-          [4, 4, 4, 5, 5, 5, 6, 6, 6], [4, 4, 4, 5, 5, 5, 6, 6, 6], [4, 4, 4, 5, 5, 5, 6, 6, 6],
-          [7, 7, 7, 8, 8, 8, 9, 9, 9], [7, 7, 7, 8, 8, 8, 9, 9, 9], [7, 7, 7, 8, 8, 8, 9, 9, 9]])},
-
-    # the case that single pixel maps to multiple earth coords, and the resolution is provided explicitly
-    {'pixel_coord': np.array([[3, 2, 1], [4, 5, 6], [7, 8, 9]]),
-     'meta': {'archive': 'ISA', 'year': 2023, 'observatory': 'Aigean', 'instrument': 'Fand', 'date': '2022-12-05',
-              'time': '19:22:10', 'xcoords': [300.0, 306.0], 'ycoords': [50.0, 56.0], 'resolution': 2},
-     'resolution': 3,
-     'expect_earth_coord': np.array(
-         [[3, 3, 3, 2, 2, 2, 1, 1, 1], [3, 3, 3, 2, 2, 2, 1, 1, 1], [3, 3, 3, 2, 2, 2, 1, 1, 1],
-          [4, 4, 4, 5, 5, 5, 6, 6, 6], [4, 4, 4, 5, 5, 5, 6, 6, 6], [4, 4, 4, 5, 5, 5, 6, 6, 6],
-          [7, 7, 7, 8, 8, 8, 9, 9, 9], [7, 7, 7, 8, 8, 8, 9, 9, 9], [7, 7, 7, 8, 8, 8, 9, 9, 9]])},
-
-    # the case that multiple pixel maps to single earth coords, and the resolution is provided by the meta
-    {'pixel_coord': np.array([[2, 3, 3, 4, 6, 7, 8, 9], [3, 4, 7, 8, 9, 10, 11, 12], [9, 9, 8, 7, 7, 6, 10, 11],
-                              [14, 50, 40, 70, 9, 8, 1, 2], [9, 8, 7, 6, 5, 4, 3, 2], [4, 4, 3, 2, 1, 1, 2, 3]]),
-     'meta': {'archive': 'ISA', 'year': 2023, 'observatory': 'Aigean', 'instrument': 'Fand', 'date': '2022-12-05',
-              'time': '19:22:10', 'xcoords': [300.0, 302.7], 'ycoords': [50.0, 52.0], 'resolution': 1/3},
-     'resolution': None,
-     'expect_earth_coord': np.array([[2, 4, 8], [14, 70, 1]])},
-
-    # the case that multiple pixel maps to single earth coords, and the resolution is provided explicitly
-    {'pixel_coord': np.array([[2, 3, 3, 4, 6, 7, 8, 9], [3, 4, 7, 8, 9, 10, 11, 12], [9, 9, 8, 7, 7, 6, 10, 11],
-                              [14, 50, 40, 70, 9, 8, 1, 2], [9, 8, 7, 6, 5, 4, 3, 2], [4, 4, 3, 2, 1, 1, 2, 3]]),
-     'meta': {'archive': 'ISA', 'year': 2023, 'observatory': 'Aigean', 'instrument': 'Fand', 'date': '2022-12-05',
-              'time': '19:22:10', 'xcoords': [300.0, 306.0], 'ycoords': [50.0, 56.0], 'resolution': 2},
-     'resolution': 1/3,
-     'expect_earth_coord': np.array([[2, 4, 8], [14, 70, 1]])}
+@pytest.mark.parametrize('date, instrument', [
+    ("2023-01-04", 'lir'),
+    ("2023-01-06", 'manannan')
 ])
-def test_pixel_to_earth(pixel_coord, meta, resolution, expect_earth_coord):
-    """Testing the pixel_to_earth() function for converting the image under pixel coord to the image under earth coord.
+def test_conversion(date, instrument):
+    """Testing the pixel_to_earth() and earth_to_pixel() functions
     """
-    ...
+    query = net.query_isa(date, date, instrument)
+    net.download_isa(query[0]['filename'])
 
+    satmap = get_satmap(query[0]['filename'])
 
-@pytest.mark.parametrize('earth_coord, meta, resolution, expect_pixel_coord', [
-    # the case that single pixel maps to multiple earth coords, and the resolution is provided by the meta
-    {'earth_coord': np.array(
-         [[3, 3, 3, 2, 2, 2, 1, 1, 1], [3, 3, 3, 2, 2, 2, 1, 1, 1], [3, 3, 3, 2, 2, 2, 1, 1, 1],
-          [4, 4, 4, 5, 5, 5, 6, 6, 6], [4, 4, 4, 5, 5, 5, 6, 6, 6], [4, 4, 4, 5, 5, 5, 6, 6, 6],
-          [7, 7, 7, 9, 9, 9, 10, 10, 10], [7, 7, 7, 9, 9, 9, 10, 10, 10], [7, 7, 7, 9, 9, 9, 10, 10, 10]]),
-     'meta': {'archive': 'ISA', 'year': 2023, 'observatory': 'Aigean', 'instrument': 'Fand', 'date': '2022-12-05',
-              'time': '19:22:10', 'xcoords': [300.0, 309.0], 'ycoords': [50.0, 59.0], 'resolution': 3},
-     'resolution': None,
-     'expected_pixel_coord': np.array([[3, 2, 1], [4, 5, 6], [7, 9, 10]])},
+    estimated_earth = pixel_to_earth(satmap.data, satmap.meta)
 
-    # the case that single pixel maps to multiple earth coords, and the resolution is provided explicitly
-    {'earth_coord': np.array(
-         [[3, 3, 3, 2, 2, 2, 1, 1, 1], [3, 3, 3, 2, 2, 2, 1, 1, 1], [3, 3, 3, 2, 2, 2, 1, 1, 1],
-          [4, 4, 4, 5, 5, 5, 6, 6, 6], [4, 4, 4, 5, 5, 5, 6, 6, 6], [4, 4, 4, 5, 5, 5, 6, 6, 6],
-          [7, 7, 7, 9, 9, 9, 10, 10, 10], [7, 7, 7, 9, 9, 9, 10, 10, 10], [7, 7, 7, 9, 9, 9, 10, 10, 10]]),
-     'meta': {'archive': 'ISA', 'year': 2023, 'observatory': 'Aigean', 'instrument': 'Fand', 'date': '2022-12-05',
-              'time': '19:22:10', 'xcoords': [300.0, 306.0], 'ycoords': [50.0, 56.0], 'resolution': 2},
-     'resolution': 3,
-     'expected_pixel_coord': np.array([[3, 2, 1], [4, 5, 6], [7, 9, 10]])},
+    assert np.array(satmap.data).all() == earth_to_pixel(estimated_earth['earthCoord'], satmap.meta).all()
+    assert len(estimated_earth['earthCoord']) == satmap.meta['ycoords'][1] - satmap.meta['ycoords'][0]
+    assert len(estimated_earth['earthCoord'][0]) == satmap.meta['xcoords'][1] - satmap.meta['xcoords'][0]
 
-    # the case that multiple pixel maps to single earth coords, and the resolution is provided by the meta
-    {'earth_coord': np.array([[2, 4, 8], [14, 70, 1]]),
-     'meta': {'archive': 'ISA', 'year': 2023, 'observatory': 'Aigean', 'instrument': 'Fand', 'date': '2022-12-05',
-              'time': '19:22:10', 'xcoords': [300.0, 302.7], 'ycoords': [50.0, 52.0], 'resolution': 1/3},
-     'resolution': None,
-     'expected_pixel_coord': np.array([[2, 2, 2, 4, 4, 4, 8, 8], [2, 2, 2, 4, 4, 4, 8, 8], [2, 2, 2, 4, 4, 4, 8, 8],
-                              [14, 14, 14, 70, 70, 70, 1, 1], [14, 14, 14, 70, 70, 70, 1, 1], [14, 14, 14, 70, 70, 70, 1, 1]])},
-
-    # the case that multiple pixel maps to single earth coords, and the resolution is provided explicitly
-    {'earth_coord': np.array([[2, 4, 8], [14, 70, 1]]),
-     'meta': {'archive': 'ISA', 'year': 2023, 'observatory': 'Aigean', 'instrument': 'Fand', 'date': '2022-12-05',
-              'time': '19:22:10', 'xcoords': [300.0, 306.0], 'ycoords': [50.0, 56.0], 'resolution': 2},
-     'resolution': 1/3,
-     'expected_pixel_coord': np.array([[2, 2, 2, 4, 4, 4, 8, 8], [2, 2, 2, 4, 4, 4, 8, 8], [2, 2, 2, 4, 4, 4, 8, 8],
-                              [14, 14, 14, 70, 70, 70, 1, 1], [14, 14, 14, 70, 70, 70, 1, 1], [14, 14, 14, 70, 70, 70, 1, 1]])}
-])
-def test_earth_to_pixel(earth_coord, meta, resolution, expect_pixel_coord):
-    """Testing the earth_to_pixel() function for converting the image under earth coord to the image under pixel coord.
-    """
-    ...
+if __name__ == '__main__':
+   pytest.main(["-s", "-v", "test_utils.py"])
